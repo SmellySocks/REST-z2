cc708c3a7b807b812a10e5463c16e0991266068d
SmellySocks <michal96hyla@gmail.com>
2021-05-07 01:29:33 +0200

    Task1 Done

--- src/main/java/edu/pja/sri/s14616/sri02/rest/EmployeeController.java
+++ src/main/java/edu/pja/sri/s14616/sri02/rest/EmployeeController.java
@@ -4,13 +4,13 @@ import edu.pja.sri.s14616.sri02.dto.EmployeeDto;
 import edu.pja.sri.s14616.sri02.model.Employee;
 import edu.pja.sri.s14616.sri02.repo.EmployeeRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
@@ -52,5 +52,37 @@ public class EmployeeController {
         }
         else
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+    @PutMapping
+    public ResponseEntity saveNewEmployee(@RequestBody EmployeeDto emp){
+        Employee entity = convertToEntity(emp);
+        employeeRepository.save(entity);
+        HttpHeaders headers = new HttpHeaders();
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(entity.getId())
+                .toUri();
+        headers.add("Location", location.toString());
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+
+    }
+    @PutMapping("/{empId}")
+    public ResponseEntity updateEmployee(@PathVariable Long empId, @RequestBody EmployeeDto employeeDto){
+        Optional<Employee> currentEmp =
+                employeeRepository.findById(empId);
+        if(currentEmp.isPresent()){
+            employeeDto.setId(empId);
+            Employee entity = convertToEntity(employeeDto);
+            employeeRepository.save(entity);
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+    @DeleteMapping("/{empId}")
+    public ResponseEntity deleteEmployee(@PathVariable Long empId){
+        employeeRepository.deleteById(empId);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
